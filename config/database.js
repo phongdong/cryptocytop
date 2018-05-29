c2a6192ef48ebca37c278550bab8ccfc7bf28645
phongdong <phongdongt@gmail.com>
2018-05-28 17:02:46 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,4 @@
 module.exports = {
-    //database: 'mongodb://localhost:27017/cryptocytop',
-    //database: 'mongodb://phong:phonG@ds231205.mlab.com:31205/nmovies',
-    database: 'mongodb://cryptouser:Cryptouser9@ds235840.mlab.com:35840/cryptocytop',
+    database: 'mongodb://localhost:27017/cryptocytop',
     secret: 'yoursecret'
 }
