d073f6b40a0a38f48a6ef8cd662e542e78b0d8cc
Phong Dong <phongdongt@gmail.com>
2018-05-28 17:27:20 -0700

    testing

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
     //database: 'mongodb://localhost:27017/cryptocytop',
     //database: 'mongodb://phong:phonG@ds231205.mlab.com:31205/nmovies',
+    //
     database: 'mongodb://cryptouser:Cryptouser9@ds235840.mlab.com:35840/cryptocytop',
     secret: 'yoursecret'
 }
